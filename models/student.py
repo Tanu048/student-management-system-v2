3bca8ca166554062a49b221ee95f165957147b02
Tanu048 <tanu.patidar.d@gmail.com>
2026-01-15 23:21:38 +0530

    made the instance arguments private

--- models/student.py
+++ models/student.py
@@ -6,24 +6,23 @@ from datetime import datetime
 class Student:
 
     def __init__(self, name: str, standard: str, roll_number: str, marks: list[int]):
-        """
-        Initializes a Student instance with a unique creation timestamp.
-        """
-        try:
-            self.name = name
-            self.standard = standard
-            self.roll_number = roll_number
-            self.marks = marks
-            self.date_created = datetime.now().strftime("%d-%m-%y %H:%M:%S")
-        except ValueError:
-            print("Enter valid values!")
+        """Initialize a Student instance with validation and timestamp."""
+        if not name or not isinstance(name, str):
+            raise ValueError("Name must be non-empty string")
+        
+        self._name = name
+        self._standard = standard
+        self._roll_number = roll_number
+        self._marks = marks
+        self._date_created = datetime.now().strftime("%d-%m-%y %H:%M:%S")
+        
 
     def to_dict(self) -> dict:
         """Returns a dictionary with a fixed order of fields."""
         return {
-            "name": self.name,
-            "standard": self.standard,
-            "roll_number": self.roll_number,
-            "marks": self.marks,
-            "date_created": self.date_created,
+            "name": self._name,
+            "standard": self._standard,
+            "roll_number": self._roll_number,
+            "marks": self._marks,
+            "date_created": self._date_created,
         }
