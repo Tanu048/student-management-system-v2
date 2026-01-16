288b8eb97b85016e103ed015957de0c5f26f1c73
Tanu048 <tanu.patidar.d@gmail.com>
2026-01-16 10:59:07 +0530

    redid logic using oop

--- storage_handler/json_handler.py
+++ storage_handler/json_handler.py
@@ -3,54 +3,52 @@
 from pathlib import Path
 import json
 
-parent_dir = (Path(__file__).resolve().parent.parent)  # added path to the root directory in resolve method so that the code works from any destination and the adress is not just relative
-data_file = Path(parent_dir) / "data" / "students.json"
+from student_logging.student_log import LogInfo
 
-def get_data() -> dict:
-    """
-    Fetches the current student dataset from the JSON file.
+class StudentJson:
+    # added path to the root directory in resolve method so that the code works from any destination and the adress is not just relative
+    _data_file = Path((Path(__file__).resolve().parent.parent)) / "data" / "students.json"
 
-    Returns:
-        dict: A dictionary containing student records. Returns empty dict if file is missing/corrupt.
-    """
-    if not data_file.exists():
-        return {}
+    def get_data() -> dict:
+        """
+        Fetches the current student dataset from the JSON file.
+        Returns:
+            dict: A dictionary containing student records. Returns empty dict if file is missing/corrupt.
+        """
+        try:
+            with open(StudentJson._data_file) as f:
+                data = json.load(f)
+        except json.JSONDecodeError:
+            LogInfo.log_error("File not accessible")
+            return {}
+        except FileNotFoundError:
+            LogInfo.log_error("File doesn't exist")
+            return {}
 
-    try:
-        with open(data_file) as f:
-            data = json.load(f)
-    except json.JSONDecodeError:
-        print("Error: File not accessible \n")
-        return {}
-    except FileNotFoundError:
-        print("Error: File doesn't access\n")
-        return {}
-
-    return data
+        return data
 
 
-def set_data(data: dict) -> bool:
-    """
-    Persists the student dataset to a JSON file using an atomic-style write.
-
-    Args:
-        data (dict): The complete dataset to be saved.
-    Returns:
-        bool: True if save was successful, False otherwise.
-    """
-    try:
-        data_file.parent.mkdir(
-            parents=True, exist_ok=True
-        )  # here the parent of the json file checks for the existance of directory and parent of directory as arguments are used and won't throw error even if file exists already
-       
-        temp_file = data_file.with_suffix(".tmp")
-        with open(temp_file, "w") as f:
-            json.dump(data, f, indent=4)
-        temp_file.replace(data_file)
-        return True
-    except TypeError as e:
-        print(f"Type error in JSON serialization: {e}\n")
-        return False
-    except (ValueError, OSError) as e:
-        print(f"Cannot write to file: {e}\n")
-        return False
+    def set_data(data: dict) -> bool:
+        """
+        Persists the student dataset to a JSON file using an atomic-style write.
+        Args:
+            data (dict): The complete dataset to be saved.
+        Returns:
+            bool: True if save was successful, False otherwise.
+        """
+        try:
+            StudentJson._data_file.parent.mkdir(
+                parents=True, exist_ok=True
+            )  # here the parent of the json file checks for the existance of directory and parent of directory as arguments are used and won't throw error even if file exists already
+        
+            temp_file = StudentJson._data_file.with_suffix(".tmp")
+            with open(temp_file, "w") as f:
+                json.dump(data, f, indent=4)
+            temp_file.replace(StudentJson._data_file)
+            return True    
+        except TypeError as e:
+            LogInfo.log_error(f"Type error in JSON serialization: {e}\n")
+            return False
+        except (ValueError, OSError) as e:
+            LogInfo.log_error(f"Cannot write to file: {e}\n")
+            return False
