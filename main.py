eb4e04def3c6597159a5940e63c00da728488aab
Tanu048 <tanu.patidar.d@gmail.com>
2026-01-20 22:40:24 +0530

    feat: add student search via query parameters

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Query
+from pydantic import BaseModel, Field
 from typing import List
 
 from models.student import Student
@@ -10,19 +10,16 @@ manager = StudentManager()
 
 
 class ValidateStudent(BaseModel):
-    name: str
-    std: str
-    roll: str
-    marks: list[int]
+    name: str = Field(..., min_length=1)
+    std: str = Field(..., gt=0)
+    roll: str = Field(..., gt=0)
+    marks: List[int] = Field(..., min_size=1)
 
-
-class ValidatePer(BaseModel):
-    std: str
-    roll: str
-
+class MessageResponse(BaseModel):
+    message: str
 
 @app.post("/add_students")
-def add_student(student: ValidateStudent):
+def add_student(student: ValidateStudent) -> MessageResponse:
     try:
         manager.add_student(
             name=student.name, std=student.std, roll=student.roll, marks=student.marks
@@ -33,21 +30,40 @@ def add_student(student: ValidateStudent):
 
 
 @app.get("/view_students")
-def get_student():
+def get_student() -> dict:
     return manager.view_list()
 
 
 @app.get("/students/search/by_roll")
-def get_student_by_roll(std, roll):
+def get_student_by_roll(
+    std: str = Query(..., min_length=1), roll: str = Query(..., min_length=1)
+) -> dict:
     try:
         return manager.search_by_roll(std, roll)
     except ValueError as e:
-        raise e(status_code=422, detail=str(e))
+        raise Exception (status_code=400, detail=str(e))
 
 
 @app.get("/students/search/by_name")
-def get_student_by_roll(name: str):
+def get_student_by_roll(name: str = Query(..., min_length=1)) -> dict:
     try:
         return manager.search_by_name(name)
     except ValueError as e:
-        raise e(status_code=422, detail=str(e))
+        raise Exception(status_code=400, detail=str(e))
+
+
+@app.get("/percent_student")
+def get_percentage(std: str = Query(..., min_length=1), roll: str = Query(..., min_length=1)) -> dict:
+    try:
+        return manager.per_calc(std, roll)
+    except ValueError as e:
+        raise Exception(status_code=400, detail=str(e))
+
+
+@app.delete("/delete_students")
+def delete_student(std: str = Query(..., min_length=1), roll: str = Query(..., min_length=1)) -> MessageResponse:
+    try:
+        manager.delete_student(std, roll)
+        return {"messagne": "student deleted"}
+    except :
+        raise Exception(status_code=404) #, detail=str(e))
