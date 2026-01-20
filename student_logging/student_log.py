c166cbb571d0f0697851a93e5d96a49c983013f5
Tanu048 <tanu.patidar.d@gmail.com>
2026-01-20 22:45:12 +0530

    log caller filename instead of logger module

--- student_logging/student_log.py
+++ student_logging/student_log.py
@@ -2,10 +2,10 @@ import logging
 
 class LogInfo:
 
-    logger = logging.getLogger("Administrator")
+    logger = logging.getLogger("student_app")
     logger.setLevel(logging.INFO)
 
-    formatter = logging.Formatter("%(name)s : %(levelname)s : %(message)s : %(asctime)s")
+    formatter = logging.Formatter("%(filename)s:%(lineno)d | %(levelname)s | %(message)s | %(asctime)s")
 
     file_handler = logging.FileHandler("student_logging/students.log")
     file_handler.setFormatter(formatter)
@@ -14,10 +14,10 @@ class LogInfo:
     logger.addHandler(file_handler)
 
     def log_error(message:str):
-        return LogInfo.logger.error(message)
+        return LogInfo.logger.error(message, stacklevel=3)
 
     def log_info(message:str):
-        return LogInfo.logger.info(message)
+        return LogInfo.logger.info(message, stacklevel=3)
     
     def log_warning(message:str):
-        return LogInfo.logger.warning(message)
+        return LogInfo.logger.warning(message, stacklevel=3)
